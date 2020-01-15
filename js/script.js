6e0bb5d96707bd00ff889f01b668d1f4d7013ff0
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-01-15 14:38:05 +0100

    Restart
    funzione per generare random + funzione per controllare elemento array + generazione 16 senza duplicati fatto

--- js/script.js
+++ js/script.js
@@ -2,35 +2,44 @@
 // In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// BONUS: all’inizio il software richiede anche una difficoltà all’utente
+// che cambia il range di numeri casuali.
+// Con difficoltà 0=> da 1 a 100,
+// con difficoltà 1 => da 1 a 80
+// con difficoltà 2=> da 1 a 50
 
-function getRandomNum(min, max) {
+// funzione per generare numeri random
+function genRandomNum (min, max) {
   var random = Math.floor(Math.random() * (max - min + 1) ) + min;
   return random;
 }
 
-var youLoseList = [];
-for (var r = 0; r < 16; r++) {
-  var cpuRandomNum = getRandomNum(1, 100);
-  youLoseList.push(cpuRandomNum);
-  console.log(cpuRandomNum);
-}
-console.log(youLoseList);
-
-
-var sameNum = false;
-for (var i = 0; i < 84; i++) {
-  if (sameNum == false) {
-    var userNum = parseInt(prompt("Numero"));
-    for (var j = 0; j < youLoseList.length; j++) {
-      if (userNum == youLoseList[j]) {
-        sameNum = true;
-        j = youLoseList.length;
-      } else {
-        sameNum = false;
-      }
+// funzione per controllare elemento presente in array
+function isInArray(array, element) {
+  var i = 0;
+  var sameNum = false;
+  while (i < array.length && sameNum == false) {
+    if (array[i] == element) {
+      sameNum = true;
     }
-  } else {
-    console.log("Game Over. Il tuo punteggio è di " + i);
-    i = 84;
+    i++;
+  }
+  return sameNum;
+}
+
+// array per salvare numeri random
+var randomNumList = [];
+
+// array per salvare numeri utente
+var userNumList = [];
+
+// generare 16 numeri random
+while (randomNumList.length < 16) {
+  var randomNum = genRandomNum(1, 100);
+  console.log(randomNum);
+  var alreadyIn = isInArray(randomNumList, randomNum);
+  if (alreadyIn == false){
+    randomNumList.push((randomNum));
   }
 }
+console.log(randomNumList.sort());
---
+++ old js/old.js
@@ -0,0 +1,39 @@
+// Il computer deve generare 16 numeri casuali da 1 a 100.
+// In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+// OLD MAD PROJECT
+
+
+function getRandomNum(min, max) {
+  var random = Math.floor(Math.random() * (max - min + 1) ) + min;
+  return random;
+}
+
+var youLoseList = [];
+for (var r = 0; r < 16; r++) {
+  var cpuRandomNum = getRandomNum(1, 100);
+  youLoseList.push(cpuRandomNum);
+  console.log(cpuRandomNum);
+}
+console.log(youLoseList);
+
+
+var sameNum = false;
+for (var i = 0; i < 84; i++) {
+  if (sameNum == false) {
+    var userNum = parseInt(prompt("Numero"));
+    for (var j = 0; j < youLoseList.length; j++) {
+      if (userNum == youLoseList[j]) {
+        sameNum = true;
+        j = youLoseList.length;
+      } else {
+        sameNum = false;
+      }
+    }
+  } else {
+    console.log("Game Over. Il tuo punteggio è di " + i);
+    i = 84;
+  }
+}
