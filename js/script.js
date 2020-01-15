66029434c97b62752940ecc15553f5651ae43cb1
Simone Tarantino <questamailnonmiserveaniente@gmail.com>
2020-01-15 15:46:58 +0100

    Completo

--- js/script.js
+++ js/script.js
@@ -8,25 +8,6 @@
 // con difficoltà 1 => da 1 a 80
 // con difficoltà 2=> da 1 a 50
 
-// funzione per generare numeri random
-function genRandomNum (min, max) {
-  var random = Math.floor(Math.random() * (max - min + 1) ) + min;
-  return random;
-}
-
-// funzione per controllare elemento presente in array
-function isInArray(array, element) {
-  var i = 0;
-  var sameNum = false;
-  while (i < array.length && sameNum == false) {
-    if (array[i] == element) {
-      sameNum = true;
-    }
-    i++;
-  }
-  return sameNum;
-}
-
 // array per salvare numeri random
 var randomNumList = [];
 
@@ -46,20 +27,80 @@ console.log(randomNumList.sort());
 
 // variabile per il numero di tentativi per difficoltà
 var attempts = 0;
-var maxAttemptsEz = 10;
+var maxAttempts = 0;
+
+// variabile per verificare la fine parita / vittoria
+var youWin = false;
+
+// scelta difficoltà
+do {
+  var levelChoice = prompt("Scegli la difficoltà da 0 a 2");
+  var levelInRange = isInRange(0, 2, levelChoice);
+  if (levelInRange == false) {
+    alert("Inserisci un valore corretto");
+  } else if (levelChoice == 0) {
+    maxAttempts = 84;
+    var maxNumLvl = 100;
+  } else if (levelChoice == 1) {
+    maxAttempts = 64;
+    var maxNumLvl = 80;
+  } else if (levelChoice == 2) {
+    maxAttempts = 50;
+    var maxNumLvl = 34;
+  }
+} while (levelInRange == false);
+
+alert("Hai scelto la modalità con " + maxNumLvl + " numeri e " + maxAttempts + " tentativi massimi prima del punteggio massimo! Iniziamo!");
 
 // faccio inserire numero all'utente
 do {
-  var userNum = parseInt(prompt("Inserisci un numero da 1 a 100"));
-  var youWin = isInArray(randomNumList, userNum);
-  userNumList.push(userNum);
-  attempts++;
-  // se il numero che inserisce è presente nella lista dei numeri random il gioco finisce
-  if (youWin == true) {
-    alert("Game Over. Il tuo punteggio è di " + (attempts - 1));
-    console.log(userNumList);
-  } else if (attempts == maxAttemptsEz) {
-    alert("Complimenti! Hai ottenuto il punteggio massimo");
-    youWin = true;
+  var userNum = parseInt(prompt("Inserisci un numero da 1 a " + maxNumLvl));
+  var validationUserNum = isInRange(1, maxNumLvl, userNum);
+  var validationUserNum2 = isInArray(userNumList, userNum);
+  console.log(validationUserNum);
+  if (validationUserNum == true && validationUserNum2 == false) {
+    youWin = isInArray(randomNumList, userNum);
+    userNumList.push(userNum);
+    attempts++;
+    // se il numero che inserisce è presente nella lista dei numeri random il gioco finisce
+    if (youWin == true) {
+      alert("Game Over. Il tuo punteggio è di " + (attempts - 1));
+      console.log(userNumList);
+      // se l'utente arriva alla fine dei tentativi senza mai sbagliare numero
+    } else if (attempts == maxAttempts) {
+      alert("Complimenti! Hai ottenuto il punteggio massimo");
+      youWin = true;
+    }
+  } else {
+    alert("Il numero che hai inserito non rispetta le condizioni richieste, inseriscine un altro");
   }
 } while (youWin == false);
+
+
+// funzione per generare numeri random
+function genRandomNum(min, max) {
+  var random = Math.floor(Math.random() * (max - min + 1) ) + min;
+  return random;
+}
+
+// funzione per controllare elemento presente in array
+function isInArray(array, element) {
+  var i = 0;
+  var sameNum = false;
+  while (i < array.length && sameNum == false) {
+    if (array[i] == element) {
+      sameNum = true;
+    }
+    i++;
+  }
+  return sameNum;
+}
+
+// funzione per verificare range numero
+function isInRange(min, max, element) {
+  var inRange = false;
+  if (element >= min && element <= max) {
+    inRange = true;
+  }
+  return inRange;
+}
